4f60689edac53fd9cf2ac72ce96dcbb85e1be1c2
Muhammad Ali <muhammad_ali@workmail.com>
2024-06-29 08:50:11 +0500

    created essentials files.

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
